6c7073e02f385df0721c18ab93a8006e0a9a7208
AndresRedondo87 <66962177+AndresRedondo87@users.noreply.github.com>
2020-06-24 19:05:01 +0200

    Projektdateien hinzufügen.

---
+++ KlassenDefinieren/Auto.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KlassenDefinieren
+{
+    public class Auto
+    {
+        public string marke;
+        public int alterInJahren;
+
+        public void BeschleunigeAuf100KmH()
+        {
+            if (this.IstSchnell())       //hier innerhalb Auto Klasse dürfen wir doch IstSchnell aufrufen.
+            {
+                Console.WriteLine("Habe schnell beschleunigt!");
+            }
+            else
+            {
+                Console.WriteLine("Habe normal beschleunigt!!YEAH!!");
+            }
+
+        }
+
+        private bool IstSchnell()   //private Methoden wird nur innerhalb von diese Klasse verfügbar
+        {
+            if (marke == "Porsche")
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+
+}
+
---
+++ KlassenDefinieren/Program.cs
@@ -0,0 +1,58 @@
+﻿/*****************************************
+ TIC-TAC-TOE
+  A   B   C
+0   |   |
+ ---+---+---
+1   |   |
+ ---+---+---
+2   |   |
+     
+
+  A   B   C
+0 X |   |
+ ---+---+---
+1   | O |
+ ---+---+---
+2   |   |
+
+     
+    Klassen - Kandidaten: Spieler, SpielKoordinaten, Feld, Spielstein, Spielbrett, Zug
+    Attribute Kandidaten: Symbol des Spielsteins
+
+    Verhalten: Spielstarten, ziehen im Wechsel, Platzieren, Anzeigen, Neuanzeigen, NeuesSpielBeginnen(neu), ProgrammVerlassen(Ende), MeldungFuerSpielEnde.
+
+    Erste Klasse aus den Kandidaten: Spielbrett
+    .... Beispiel Abgebrochen?
+
+*****************************************/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KlassenDefinieren
+{
+    //auto Klasse haben wirvon hier rausgeschnitten, dann rechte maus auf Klassendefinieren/Hinzufügen/Klasse und da die klasse einfach umbennant. 
+    //Jetzt ist ein getrennte Klassen Datei Auto.cs
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Auto meinAuto = new Auto();     //jedes Objekt ist eine neue Instanz von die Auto Klasse: mein und dein Auto
+            Auto deinAuto = new Auto();
+
+            //die methode istSchnell ist nicht unter meinAuto verfügbar, da es private definiert worden ist.
+            meinAuto.alterInJahren = 13;
+            meinAuto.marke = "Audi";
+
+            deinAuto.alterInJahren = 1;
+            deinAuto.marke = "Porsche";
+
+            meinAuto.BeschleunigeAuf100KmH();
+            deinAuto.BeschleunigeAuf100KmH();
+        }
+    }
+}
