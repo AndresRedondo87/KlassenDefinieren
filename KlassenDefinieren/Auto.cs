edee2870d6ef3a56b375f0e1d212bcc68a28330d
AndresRedondo87 <66962177+AndresRedondo87@users.noreply.github.com>
2020-08-31 13:40:33 +0200

    Revert "Video 74 Properties oder Eigenschaften - Kein direkter Zugriff auf attributen bitte!!"
    This reverts commit 2f5e7f1d9526a8697ea3a8505b2e8845d293338a.

--- KlassenDefinieren/Auto.cs
+++ KlassenDefinieren/Auto.cs
@@ -11,54 +11,20 @@ namespace KlassenDefinieren
         /// 1 muss zugriffsmodifizierer:
         /// Public muss definiert werden, sonst bleibt es internal als default.
         /// 2 muss class und dann Klassenname(grossgeschrieben)
+        /// 
 
-        ///ATTRIBUTEN: ---> PROPERTIES (EIGENSCHAFTEN)
+        ///ATTRIBUTEN:
         /// 1 muss zugriffsmodifizierer:
         /// Public muss definiert werden, sonst bleibt es internal als default.
         /// dann variablentyp und dann variablenname, kleingechrieben.
-        /// private string marke;   //Updated in Video 73, da es nicht geandert sein konnen sollte
-        ///public int alterInJahren;
-        ///private double aktuelleGeschwindigkeitInKM;
+        private string marke;   //Updated in Video 73, da es nicht geandert sein konnen sollte
+        public int alterInJahren;
+        private double aktuelleGeschwindigkeitInKM;
         ///aktuelle geschw. als private weil wir es nur innen brauchen, wird nur von innen geaendert, nie von ausserhalb von der klasse.
         /// Video 72 - Das waere ein Beispiel fuer Datenkapselung.
         /// Wenn von extern den Attribut geaendert worden ist und wir muessen es zum Beispiel entfernen, müssen wir viel mehr aendern als wenn es nur gekapselt waere...
         /// So vermeiden wir dass es von aussen geaendert wird und wird flexibler wenn wir irgendwie die Inhaelte nur durch Methoden Aendern können
         /// Ich vermute hier kommen alle die get set erklaerungen... mal sehen.
-        /// Video 74 Properties oder Eigenschaften - Kein direkter Zugriff auf attributen bitte!! -->get und set.
-        /// Propfull+tab+tab
-        /// dann Attribut umbenennen und Eigenschaft Name ist das gleiche mit erste Grossgeschrieben.
-        ///private int alterInJahren;  //Attribut, ist private.
-        ///public int AlterInJahren    //Eigenschaft, ist Public
-        ///{
-        ///    get { return alterInJahren; }   //von aussen lesen
-        ///    set { alterInJahren = value; }  //von aussen aendern
-        ///}
-        /// extra auf Video 74 - Property auch fuer aktuelleGeschwindigkeitInKM
-        ///Video 74 KURZSCHREIBWEISE mit prop+tab+tab, Enorme Vorteile, kann man hier einfacher aendern als ein Attribut,
-        ///so immer viel besser Eigenschaften verwenden anstatt direkte Attributen um besser arbeiten zu koennen.
-        ///dannach koennen auch besser die grenzen und generell alles bearbeiten von der Attribut via seine Property(Eigenschaft)
-        ///immer nur in die entsprechende klasse rein, und nicht ueberall in unserer Programm
-        public int AlterInJahren { get; set; }
-        public double aktuelleGeschwindigkeitInKM { get; set; }
-
-        private string marke;
-
-        public string Marke
-        {
-            get { return marke; }
-            set
-            { // wir koennen auch die eingegebenen werte ueberpruefen, zB. hier akzeptieren wir nur Audi und Prosche als akzeptierte Marken:
-                if ((value == "Porsche") || (value == "Audi"))
-                {
-                    marke = value;
-                }
-                else
-                {
-                    marke = "nicht unterstütze Marke";
-                }
-            }
-        }
-
 
         /// Video 70 - MiniUEbung, erweitere die Klasse Auto.
         /// -Aktuelle Geschwindigkeit speichern--DATEN
@@ -75,8 +41,16 @@ namespace KlassenDefinieren
         /// 
         public Auto(string autoMarke)
         {
-            Marke = autoMarke;
-            AlterInJahren = 0;
+            // wir koennen auch die eingegebenen werte ueberpruefen, zB. hier akzeptieren wir nur Audi und Prosche als akzeptierte Marken:
+            if((autoMarke == "Porsche")|| (autoMarke == "Audi"))
+            {
+                marke = autoMarke;
+            }
+            else
+            {
+                marke = "nicht unterstütze Marke";
+            }
+            alterInJahren = 0;
             aktuelleGeschwindigkeitInKM = 0;
         }
 
