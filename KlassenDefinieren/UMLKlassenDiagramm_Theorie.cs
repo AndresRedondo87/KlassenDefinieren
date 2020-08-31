9b261a63beaa2630e65fd7d5b7760a88153c306d
AndresRedondo87 <66962177+AndresRedondo87@users.noreply.github.com>
2020-08-31 14:24:12 +0200

    Video 76 UML Klassendiagramm - Beziehungen visualisieren

--- KlassenDefinieren/UMLKlassenDiagramm_Theorie.cs
+++ KlassenDefinieren/UMLKlassenDiagramm_Theorie.cs
@@ -27,6 +27,24 @@ namespace KlassenDefinieren
         ///   |------------------------------------|       Namen(parameter):Rueckgabewert
         ///   
 
+        /// Video 76 UML Klassendiagramm - Beziehungen visualisieren
+        /// Beziehungen sind sogar wichtiger als die Klassen selber
+        /// Beziehungen = ASSOZIAZIONEN
+        /// 
+        ///  AUTO --------- HERSTELLER
+        ///   |            /
+        ///   |           /
+        ///   |          /
+        ///   |         /
+        ///  MITARBEITER
+        ///  In schema oben werden nicht gemalt aber wichtig sind die Pfeile enden und die Beziehung inzswischen:
+        ///  Pfeile werden zeigen die Abhaengigkeiten. Je weniger abhaengigkeiten desto besser und einfacher wird diese Klasse zu evolvieren bzw. AEndern.
+        ///  In Beispiel beide Pfeile zeigen nach auto, da die beiden Hersteller und Mitarbeiter sind vom Auto abhaengig.
+        ///  Da ist auch wer weiss etwas ueber wer oder was, von Hersteller richtung Auto. 
+        ///  Der mitarbeiter hat eine Beziehung zur Auto aber das Auto weiss nichts von Mitarbeiter
+        ///  
+        ///  
+
     }
 
 }
