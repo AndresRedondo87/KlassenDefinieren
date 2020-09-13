64d473aa8231858fb8d40db6d6a73b764f30c1a5
AndresRedondo87 <66962177+AndresRedondo87@users.noreply.github.com>
2020-09-13 19:25:55 +0200

    Video 104 String im Detail Video 105 Datetime Video 106 Timespan - WICHTIG!  Berechnungen und TryParse aus string.

--- KlassenDefinieren/Program.cs
+++ KlassenDefinieren/Program.cs
@@ -85,6 +85,139 @@ namespace KlassenDefinieren
             meinAuto.AktuelleGeschwAnzeigen();
             deinAuto.AktuelleGeschwAnzeigen();
             andereAuto.AktuelleGeschwAnzeigen();
+            //Console.ReadKey();
+
+            Console.WriteLine();
+
+            ///Video 104 String im Detail
+            {
+
+                //String text = "textooo"; String ist die klasse,string ist der bezeicher von eine objekt von String
+                //String text = "textooo";
+                string text = "textooo";   // == System.String text2 
+
+                //ein string ist ein Zeichenkette, bzw eine reihenfolge von Zeichen.
+                char einzeichen = text[2];
+
+                // Ein String ist inmutable. Kann man nicht ändern.
+                // um irgendwie zu ändern, muss man ein neu string definieren
+                // INMUTABLE -> NICHT VERÄNDERBAT
+                //text[2] = 's'; //geht gar nicht
+                // es wird immer ein neuer string generiert.
+
+                string hallo = "Hallo "; 
+                string welt = "Welt";
+                string halloWelt = hallo + welt;
+                Console.WriteLine(halloWelt);
+
+                string neueZeile = "\nin einer neue Zeile";
+                Console.WriteLine(neueZeile);
+
+                //Sonderzeichen ausgeben: mit backslash(Escapesequenzen)
+
+                string neueZeile2 = "\nin einer \"neue\" Zeile.";
+                Console.WriteLine(neueZeile);
+
+                string pfad = "C:\\temp\\Ordner1zumBeispiel";
+                Console.WriteLine(pfad);
+
+                //Oder mit Verbatim string. Ein@ vor dem "" und alles wird direkt so wie sie sind ausgegeben.
+                string verbatimPfad = @"C:\temp\Ordner1zumBeispiel";
+                Console.WriteLine(verbatimPfad);
+            }
+
+            ///Video 105 Datetime
+            {
+                DateTime einBestimmterTag = new DateTime(1999, 12, 12);
+                //wir haben kein Time eingegeben, ist auch ok
+
+
+                DateTime einBestimmterTag2 = new DateTime(1999, 12, 12, 10, 55, 59);
+                Console.WriteLine("Ein Tag: " + einBestimmterTag2);    // DateTime hat ein Tostring Methode, kann man einfach implizit aufgerufen sein
+
+                Console.WriteLine("Ein Tag short: " + einBestimmterTag2.ToShortDateString());
+                Console.WriteLine("Ein Tag long: " + einBestimmterTag2.ToLongDateString());
+                Console.WriteLine("Ein Time long: " + einBestimmterTag2.ToLongTimeString());
+
+                Console.WriteLine("Ein Tag +5: " + einBestimmterTag2.AddDays(5));   // Das ist eigentlich ein andere string
+
+
+
+                DateTime jetzt =  DateTime.Now; //aktuelle Datum und Zeit hier
+                Console.WriteLine("Jetzt ist: " + jetzt);
+                Console.WriteLine("Jetzt ticks: " + jetzt.Ticks);   //ticks sind...ganz klein, "Einheit 100 Nanosekunden"
+
+                DateTime jetztUTC = DateTime.UtcNow; //aktuelle Datum und Zeit in der UTC bzw koordinierter Weltzeit (Greenwich, bzw Spanien)
+                Console.WriteLine("Jetzt in Spanien ist: " + jetztUTC);
+            }
+
+            ///Video 106 Timespan
+            /// Zeitfenster
+            {
+                DateTime einBestimmterTag = new DateTime(2019, 12, 12);
+                DateTime einAndererTag = new DateTime(2020, 07, 2);
+
+                // Erstellen von TimeSpan Objekten
+                Console.WriteLine("###Erstellen von TimeSpan Objekten! ###");
+
+                // 1 - Konstruktor
+                TimeSpan zeitSeitTagesBeginn = new TimeSpan(21,46,15);  //Zeitfenster zwischen Eingegebene Zeit und TagesAnfang 00:00... nicht so hilfreich
+
+                // 2 - statische From-Methoden
+                TimeSpan zeiVonMitternachtBisFruehstueck = TimeSpan.FromHours(10);
+
+                // 3 - Rechnen mit Datetime Objekte
+                TimeSpan zeitSeitDemLetztenVideo = einBestimmterTag - DateTime.Now; //Das hier ist praktischer.
+                // addieren und substrahieren geht ohne Probleme
+                TimeSpan eineStunde =  TimeSpan.FromHours(1);
+                TimeSpan zehnMillisekunde = TimeSpan.FromMilliseconds(10.0);
+
+                //addieren
+                TimeSpan summe = eineStunde + zehnMillisekunde;
+                TimeSpan summe2 = eineStunde.Add(zehnMillisekunde);
+
+                TimeSpan diff = eineStunde - zehnMillisekunde;
+                TimeSpan diff2 = eineStunde.Subtract(zehnMillisekunde);
+
+
+                Console.WriteLine("Zeit Seit Mitternacht: " + zeitSeitTagesBeginn);
+                Console.WriteLine("Zeit Seit Frühstück: " + zeiVonMitternachtBisFruehstueck);
+                Console.WriteLine("Zeit Seit einen Tag: " + zeitSeitDemLetztenVideo);
+
+                Console.WriteLine("Zeit summe 1 Stunde und 10 ms: " + summe);
+                Console.WriteLine("Zeit summe 1 Stunde und 10 ms: " + summe2);
+                Console.WriteLine("Zeit substr 1 Stunde und 10 ms: " + diff);
+                Console.WriteLine("Zeit substr 1 Stunde und 10 ms: " + diff2);
+
+
+                // Minutes vs TotalMinutes  AUFPASSEN: minutes zeigt nur die minuten, aber nicht die Stunden umgerechnet. 
+                //TOTALMINUTES wird die Stunden umrechnen, oder die groessere Einheit so zu sage
+                Console.WriteLine("Minutes vs TotalMinutes:");
+
+                TimeSpan dauerVonAufstehenBisFruehstueck = new TimeSpan(2, 30, 0);
+                Console.WriteLine(dauerVonAufstehenBisFruehstueck.Minutes);         
+                Console.WriteLine(dauerVonAufstehenBisFruehstueck.TotalMinutes);
+
+                //String nach Timespan konvertieren AUCH SEHR WICHTIG!!!
+                Console.WriteLine("String nach Timespan konvertieren");
+                string gueltigeDauerString = "20:35:45";
+                string ungueltigeDauerString = "20:35:75"; //75 Sekunde gibt es gar nicht
+
+
+                // TimeSpan hat auch das TryParse, gibt bool zurück wenn es machbar ist, den string zu konvertieren
+                // und auch als out, gibt uns der entsprechende Timespan Wert oder null (00:00:00)
+                TimeSpan gueltigeDauer;
+                TimeSpan.TryParse(gueltigeDauerString, out gueltigeDauer);         
+                TimeSpan ungueltigeDauer;
+
+                TimeSpan.TryParse(ungueltigeDauerString, out ungueltigeDauer);
+
+                Console.WriteLine(gueltigeDauer);
+                Console.WriteLine(ungueltigeDauer);
+
+            }
+
+
             Console.ReadKey();
         }
     }
