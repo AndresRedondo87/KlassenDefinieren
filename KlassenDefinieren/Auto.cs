68827114a1b105dc81d2ee0742ba497777a77ded
AndresRedondo87 <66962177+AndresRedondo87@users.noreply.github.com>
2020-09-13 20:16:57 +0200

    Extra info zu $ vor ein string, interpolation, sehr praktisch.

--- KlassenDefinieren/Auto.cs
+++ KlassenDefinieren/Auto.cs
@@ -88,6 +88,14 @@ namespace KlassenDefinieren
             resultat += $"aktuelle Geschwindigkeit InKM: {this.aktuelleGeschwindigkeitInKM}" + Environment.NewLine;
             return resultat;
 
+
+            //// Composite formatting:
+            //Console.WriteLine("Hello, {0}! Today is {1}, it's {2:HH:mm} now.", name, date.DayOfWeek, date);
+            //// String interpolation:
+            //Console.WriteLine($"Hello, {name}! Today is {date.DayOfWeek}, it's {date:HH:mm} now.");
+            //// Both calls produce the same output that is similar to:
+            //// Hello, Mark! Today is Wednesday, it's 19:40 now.
+
         }
 
         /// <summary>
